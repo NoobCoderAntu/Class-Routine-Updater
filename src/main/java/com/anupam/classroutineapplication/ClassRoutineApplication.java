54cc740fd1fb5dd5363dafb74c831b6f723ee356
NoobCoderAntu <anupamdey2022cse@gmail.com>
2023-09-04 23:33:48 +0600

    first commit

---
+++ src/main/java/com/anupam/classroutineapplication/ClassRoutineApplication.java
@@ -0,0 +1,13 @@
+package com.anupam.classroutineapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassRoutineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassRoutineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anupam/classroutineapplication/ClassRoutineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anupam.classroutineapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassRoutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
